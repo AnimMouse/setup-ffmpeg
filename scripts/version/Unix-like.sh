4b3251522912b4364f4a75398782bda8326038a9
AnimMouse <git@animmouse.com>
2023-02-05 15:24:20 +0800

    refactor: Use release instead of stable

--- scripts/version/Unix-like.sh
+++ scripts/version/Unix-like.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -eu
-if [ $version = stable ]
+if [ $version = release ]
 then
   if [ $RUNNER_OS = macOS ]
   then
-    latest_stable=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].latest)
+    latest_release=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].latest)
   else
-    latest_stable=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].cycle)
+    latest_release=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].cycle)
   fi
-  echo version=$latest_stable >> $GITHUB_OUTPUT
+  echo version=$latest_release >> $GITHUB_OUTPUT
 else
   echo version=$version >> $GITHUB_OUTPUT
 fi
--- scripts/version/Windows.ps1
+++ scripts/version/Windows.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
-if ($env:version -eq 'stable') {
-  $latest_stable = (Invoke-RestMethod https://endoflife.date/api/ffmpeg.json)[0].cycle
-  Add-Content $env:GITHUB_OUTPUT version=$latest_stable
+if ($env:version -eq 'release') {
+  $latest_release = (Invoke-RestMethod https://endoflife.date/api/ffmpeg.json)[0].cycle
+  Add-Content $env:GITHUB_OUTPUT version=$latest_release
 }
 else {
   Add-Content $env:GITHUB_OUTPUT version=$env:version
