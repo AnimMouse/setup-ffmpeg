a76cbba8e512c174c4b43be6fe0f7d4e1c7b6d03
Anim Mouse <git@animmouse.com>
2023-10-24 22:30:12 +0800

    fix: Use variables on specified version and use native commands for download

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -5,13 +5,11 @@ mkdir FFmpeg
 if [ $RUNNER_OS = macOS ]
 then
   if [ $version = master ]; then filename=get; else filename=ffmpeg-$version.7z; fi
-  aria2c -x 16 -o FFmpeg.7z -q https://evermeet.cx/ffmpeg/$filename
+  wget -qO FFmpeg.7z https://evermeet.cx/ffmpeg/$filename
   7z e FFmpeg.7z ffmpeg -oFFmpeg
   rm FFmpeg.7z
 else
   if [ $version = master ]; then filename=ffmpeg-master-latest-linux64-gpl.tar.xz; else filename=ffmpeg-n$version-latest-linux64-gpl-$version.tar.xz; fi
-  aria2c -x 16 -o FFmpeg.tar.xz -q $GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/$filename
-  tar -xf FFmpeg.tar.xz -C FFmpeg --strip-components 2 --no-anchored ffmpeg ffprobe
-  rm FFmpeg.tar.xz
+  wget -qO- $GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/$filename | tar -xJC FFmpeg --strip-components 2 --no-anchored ffmpeg ffprobe
 fi
 echo ::endgroup::
--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -1,8 +1,9 @@
 $ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading FFmpeg $env:version for Windows
 if ($env:version -eq 'master') {$filename = 'ffmpeg-master-latest-win64-gpl.zip'} else {$filename = "ffmpeg-n$env:version-latest-win64-gpl-$env:version.zip"}
-aria2c -x 16 -o FFmpeg.zip -q $env:GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/$filename
+Invoke-WebRequest $env:GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/$filename -OutFile FFmpeg.zip
 7z e FFmpeg.zip ffmpeg.exe ffprobe.exe -oFFmpeg -r
 Remove-Item FFmpeg.zip
 Write-Host ::endgroup::
