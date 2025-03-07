27e66fd2fe1d643b73a7c5cb105f3b4116bfb8db
Anim Mouse <git@animmouse.com>
2025-03-07 21:31:01 +0800

    fix: Use cycle release for OSXExperts.NET

--- scripts/version/Unix-like.sh
+++ scripts/version/Unix-like.sh
@@ -4,12 +4,13 @@ if [ "$version" = release ]
 then
   if [ $RUNNER_OS = macOS ]
   then
-    latest_release_macos=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].latest)
     if [ $RUNNER_ARCH = ARM64 ]
     then
+      latest_release_macos=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].cycle)
       latest_release_macos_arm64=$(basename $latest_release_macos .0)
       latest_release=${latest_release_macos_arm64/.}
     else
+      latest_release_macos=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].latest)
       if [ "$(curl -s https://evermeet.cx/ffmpeg/info/ffmpeg/$latest_release_macos | jq .code)" = 404 ]
       then
         latest_release_macos=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[1].latest)
