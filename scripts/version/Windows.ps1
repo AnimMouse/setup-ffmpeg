642005145795b4549cc29b7f90938b3e6fecda47
Anim Mouse <git@animmouse.com>
2025-08-26 01:03:24 +0800

    fix: Add logic to check if the version exists on BtbN Windows

--- scripts/version/Windows.ps1
+++ scripts/version/Windows.ps1
@@ -2,6 +2,10 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 if ($env:version -eq 'release') {
   $latest_release = (Invoke-RestMethod https://endoflife.date/api/ffmpeg.json)[0].cycle
+  Invoke-RestMethod -Uri $env:GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n$latest_release-latest-win64-gpl-$latest_release.zip -Method Head -StatusCodeVariable 'status_code' -SkipHttpErrorCheck
+  if ($status_code -eq '404') {
+    $latest_release = (Invoke-RestMethod https://endoflife.date/api/ffmpeg.json)[1].cycle
+  }
   Add-Content $env:GITHUB_OUTPUT version=$latest_release
 }
 else {
