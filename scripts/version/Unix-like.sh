1e5d9e219828f8be3a765bc12a92f7ed616190eb
Anim Mouse <git@animmouse.com>
2025-08-25 23:37:37 +0800

    fix: Add logic to check if the version exists on BtbN

--- scripts/version/Unix-like.sh
+++ scripts/version/Unix-like.sh
@@ -18,7 +18,12 @@ then
       latest_release=$(basename $latest_release_macos .0)
     fi
   else
-    latest_release=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].cycle)
+    latest_release_linux=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].cycle)
+    if [ "$(curl -sLI $GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n$latest_release_linux-latest-linux64-gpl-$latest_release_linux.tar.xz | head -n 1 | cut -d$' ' -f2)" = 404 ]
+    then
+      latest_release_linux=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[1].cycle)
+    fi
+    latest_release=$latest_release_linux
   fi
   echo "version=$latest_release" >> $GITHUB_OUTPUT
 else
