ab7cba7e26e699d92b9af91546bc35ef8eaff1f8
Anim Mouse <git@animmouse.com>
2026-06-11 02:31:31 +0800

    fix: Use complex jq queries to get version and filename on Ubuntu

--- scripts/version/Unix-like.sh
+++ scripts/version/Unix-like.sh
@@ -11,12 +11,7 @@ then
       latest_release=$(curl -s https://evermeet.cx/ffmpeg/info/ffmpeg/release | jq -r .version)
     fi
   else
-    latest_release_linux=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[0].latest)
-    if [ "$(curl -sLIo /dev/null -w %{http_code} $GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n$latest_release_linux-latest-linux64-gpl-$latest_release_linux.tar.xz)" = 404 ]
-    then
-      latest_release_linux=$(curl -s https://endoflife.date/api/ffmpeg.json | jq -r .[1].latest)
-    fi
-    latest_release=$latest_release_linux
+    latest_release=$(gh api repos/BtbN/FFmpeg-Builds/releases/latest -q '[.assets[].name | capture("^ffmpeg-(?:n)?(?<v>[0-9]+(?:\\.[0-9]+)+)") | .v] | max_by(split(".") | map(tonumber))')
   fi
   echo "version=$latest_release" >> $GITHUB_OUTPUT
 else
